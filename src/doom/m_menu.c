9db3286903a7ac0dbaaacf01d2ac6c0aa879d1fa
Orson Teodoro <orsonteodoro@hotmail.com>
2025-06-08 19:28:36 -0700

    Remove static from M_WriteText for psdoom-ng

--- src/doom/m_menu.c
+++ src/doom/m_menu.c
@@ -210,7 +210,7 @@ static void M_DrawSave(void);
 static void M_DrawSaveLoadBorder(int x,int y);
 static void M_SetupNextMenu(menu_t *menudef);
 static void M_DrawThermo(int x,int y,int thermWidth,int thermDot);
-static void M_WriteText(int x, int y, const char *string);
+void M_WriteText(int x, int y, const char *string);
 static int  M_StringWidth(const char *string);
 static int  M_StringHeight(const char *string);
 static void M_StartMessage(const char *string, void *routine, boolean input);
